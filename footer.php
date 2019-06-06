e2f0bb74cbd85f71206a12b28304b6c334f417b6
Renato Mosca <rmosca2001@hotmail.com>
2019-06-06 11:02:23 -0300

    ajustes navbar e fatiamento de blocos

---
+++ footer.php
@@ -0,0 +1,21 @@
+<footer>
+      <div class="foooter-info col-lg-6">
+        Copyright 2019 - Todos os direitos reservados.
+      </div>
+      <div class="footer-social col-lg-6">
+          <p align="right">
+          <span style="font-size: 48px; color: white;">
+            <i class="fab fa-facebook"></i>
+          </span>
+          <span style="font-size: 48px; color: white;">
+            <i class="fab fa-instagram"></i>
+          </span>
+          <span style="font-size: 48px; color: white;">
+            <i class="fab fa-twitter"></i>
+          </span>
+          <span style="font-size: 48px; color: white;">
+            <i class="fab fa-youtube-square"></i>
+          </span>          
+          </p>
+        </div>
+    </footer>
--- index.php
+++ index.php
@@ -1,87 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home - Reeduca</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <style>
-        body{
-                 
-        }
-        
-        section.fundo{
-            height: 460px; 
-            
-        }
-        .cx-esquerda{
-            height: 450px;
-            
-        }
-        .cx-direita{
-            height: 450px;
-            background-image: url("images/fundo-home.png");
-            background-size: 700px 500px;
-            background-position: center; 
-            background-repeat: no-repeat;
-        }
-        .conteudo-texto{
-            margin-top: 90px;
-        }
-        .comece{
-            /* top: 0; */
-        }
-        footer{
-            height: 110px;
-            background-color: #73C72B;
-            margin: 0 auto;
-
-        }
-        .img-money{
-            height: 110px;
-        }
-
-            
-        
-    </style>
-</head>
+<?php include "head.php"; ?>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+ 
+  <nav class="navbar navbar-expand-lg navbar-light ">
+    <a class="navbar-brand" href="#"><img src="images/logo.png"></a>
+    
+    <div class="itensMenu float-right">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Sobre</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Coleta</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Loja</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contato</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+
+      </ul>
+      
+    </div>
   </div>
 </nav>
+
+
 <section class="fundo col-lg-12">
     <div class="row">
         <div class="cx-esquerda col-lg-6">
@@ -104,8 +68,7 @@
             <div class="cx-direita col-lg-6"></div>        
     </div>
     </section>
-    <footer>
-
-    </footer>
-</body>
+    
+<?php include "footer.php"; ?>
+  </body>
 </html>
