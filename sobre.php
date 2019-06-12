8acda624864be3375b4dcde2c622558b13c45199
Lais <laisjssp@gmail.com>
2019-06-12 14:17:13 -0300

    alterações página sobre

---
+++ sobre.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "head.php"; ?>
+<body>
+
+<?php include "header.php"; ?>
+<!-- <section class="fundo col-lg-12"> -->
+<div class=sobre-page>
+        <div class="row">
+            <div class="cx-esquerda-sobre col-lg-6">
+                <div class="conteudo-texto2 col-lg-12">
+                    <h1 class="destaqueSobre">Sobre a Reeduca</h1>
+                    <p class="p-sobre">Se você em um mundo saudavel, pacífico, digno e justo para todos, então estamos ao seu lado para alcança-lo, seja aguindo nas rua, naa internet, em pequenas comunidades, nas grandes salas do poder ou nos locais mais remotos do planeta. Se tem ideias para chegarmaos lá mais rapido, queremos aprender com você. Vamos sonhar, planejar e agir juntos.</p>        
+                </div>
+            </div>   
+        </div>
+    </div>
+</div>      
+</section>
+<?php include "footer.php"; ?>
+</body>
+</html>
