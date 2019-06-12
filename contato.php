eac6c71ceacbf7f1a7691558f5e7f6d5af321eb4
Renato Mosca <rmosca2001@hotmail.com>
2019-06-12 15:10:33 -0300

    adicionando pagina contatos

---
+++ contato.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<?php 
+include "head.php"; 
+?>
+
+<body>
+
+
+<?php 
+include "header.php"; 
+?>
+
+<div class="teste">
+
+
+<div class="container-contato">
+
+<div class="row">
+
+<div class="col-6 d-flex align-items-center flex-wrap">
+
+<h1 class="fale1 col-12"> Fale Conosco
+</h1>
+
+
+</div>
+
+<div class="col-6">
+
+<section class =
+"fundo col-lg-12">
+
+<form class ="form_login1 container col-lg-8 ">
+
+<div class="form-group">
+
+<label for="nome">
+</label>
+
+<input type="text" class="contatos form-control"
+ id="nome" placeholder=" Digite seu nome completo">
+
+</div>
+
+
+<div class="form-group">
+
+<label for="idade">
+</label>
+
+<input type="number" class="contatos form-control"
+ id="idade" placeholder="Digite sua Idade">
+
+</div>
+
+
+<div class="form-group">
+
+<label for="Telefone">
+</label>
+
+<input type="number" class="contatos form-control"
+ id="telefone" placeholder="Digite um telefone de contato">
+
+</div>
+
+
+
+
+
+
+
+
+<div class="form-group">
+
+<label for="email">
+</label>
+
+<input type="email" class="contatos form-control"
+ id="email" placeholder="Nome@exemplo.com">
+
+</div>
+
+
+
+<div class =
+"botao_login">
+
+<button type="button" class="button">Enviar</button>
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+
+
+</form>
+
+</section>
+
+</div>
+<?php include "footer.php"; ?>
+</body>
+</html>
